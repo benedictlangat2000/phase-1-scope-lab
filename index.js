5e6e9019ac9269dc39f125f904233256447d463b
BenedictKipkuruiLangat <benedictlangat2000@gmail.com>
2023-07-12 01:29:50 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  if (customerName) {
+    customerName = customerName.toUpperCase();
+  }
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'initial value';
+
+function changeLeastFavoriteCustomer() {
+  // Attempting to change the constant
+  leastFavoriteCustomer = 'new value'; // This will throw an error
+}
